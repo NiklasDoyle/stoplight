1add6f53ba8c06d8fb61735c7608f3380c517269
Niklas <ndoyle1313@gmail.com>
2023-06-21 17:21:15 -0700

    Initial Commit

---
+++ Stoplight/StoplightApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoplightApp.swift
+//  Stoplight
+//
+//  Created by Niklas Doyle on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct StoplightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
